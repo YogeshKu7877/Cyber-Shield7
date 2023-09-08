c7eafc93f14e653555cd64482a10f62bfc268cc0
Nikhil-Sadhwani <137155995+Nikhil-Sadhwani@users.noreply.github.com>
2023-09-08 12:42:45 +0530

    Adding Components structure and More Dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter , Routes , Route } from "react-router-dom" ;
+
+import NavBar from "./components/NavBar";
+
+import Home from "./components/Home";
+import Education from "./components/Education";
+import HSD from "./components/HSD";
+import Tools from "./components/Tools";
+import ToolsDetail from "./components/ToolsDetail";
+import AboutUs from "./components/AboutUs";
+
+import Signin from "./components/Signin";
+import Signup from "./components/Signup";
+
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <BrowserRouter>
+        <NavBar/>
+
+        <Routes>
+
+          <Route exact path = '/' element = {<Home /> } />
+          <Route exact path = '/education' element = {<Education /> } />
+          <Route exact path = '/hsd' element = {<HSD /> } />
+          <Route exact path = '/tools' element = {<Tools /> } />
+          <Route exact path = '/toolsDetail' element = {<ToolsDetail /> } />
+          <Route exact path = '/aboutUs' element = {<AboutUs /> } />
+
+          <Route exact path = '/signin' element = {<Signin /> } />
+          <Route exact path = '/signup' element = {<Signup /> } />
+
+        </Routes>
+      
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ client/src/components/AboutUs.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function AboutUs() {
+  return (
+    <div>
+        <h1 className="text-3xl font-bold underline">
+          Hello About us
+        </h1>
+    </div>
+  )
+}
