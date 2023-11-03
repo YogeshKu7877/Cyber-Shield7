4d8d909940e83dcf6e0508a04c74a6d1f44acda2
Yogesh Kumawat <ky216732@gmail.com>
2023-11-03 16:58:29 +0530

    Initial commit

--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
 // Router
-const signupRouter  = require('./routers/signup');
-const signinRouter  = require('./routers/signin');
-const dictionaryRouter  = require('./routers/dictionary');
-const reviewRouter  = require('./routers/review');
-const reportRouter = require('./routers/reporting');
+const signupRouter = require("./routers/signup");
+const signinRouter = require("./routers/signin");
+const dictionaryRouter = require("./routers/dictionary");
+const reviewRouter = require("./routers/review");
+const reportRouter = require("./routers/reporting");
 
-const cors = require('cors');
+const cors = require("cors");
 
 const app = express();
 
@@ -17,15 +17,17 @@ app.use(cors());
 app.use(express.json());
 
 //Use Routers
-app.use("/signup" , signupRouter);
-app.use("/signin" , signinRouter);
-app.use("/dictionary" , dictionaryRouter);
-app.use("/review" , reviewRouter);
-app.use("/report" , reportRouter);
+app.use("/signup", signupRouter);
+app.use("/signin", signinRouter);
+app.use("/dictionary", dictionaryRouter);
+app.use("/review", reviewRouter);
+app.use("/report", reportRouter);
 
-//  Connecting MongoDB please enter your mongodb URL here and create collections of name : users , reviews , dictionaries , reports 
-mongoose.connect("Enter_Your_MongoDB_URL");
+//  Connecting MongoDB please enter your mongodb URL here and create collections of name : users , reviews , dictionaries , reports
+mongoose.connect(
+  "mongodb+srv://Cyber_user:poC158VetAfKK6qi@cluster0.5sbogkk.mongodb.net/?retryWrites=true&w=majority"
+);
 
-app.listen(3001 , ()=>{
-    console.log("Server is running");
+app.listen(3001, () => {
+  console.log("Server is running");
 });
